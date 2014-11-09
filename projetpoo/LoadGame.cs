8ebcf9c8045956d93330d7ba7e5b8e574248659e
Dan <dan.seeruttun@gmail.com>
2014-11-09 16:41:29 +0100

    changement sur le diagramme de classe 2eme partie

---
+++ projetpoo/LoadGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetPOO
+{
+    public class LoadGame
+    {
+        public void loadFromDisk()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
