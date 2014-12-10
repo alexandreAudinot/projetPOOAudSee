1db794f718d430a6eb9a4a85cf0ab6626a4329eb
Alexandre Audinot <alexandre.audinot@insa-rennes.fr>
2014-12-10 12:02:44 +0100

    ajout d'elements visuels

---
+++ WpfDisplay/Menu.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDisplay
+{
+    /// <summary>
+    /// Logique d'interaction pour Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
