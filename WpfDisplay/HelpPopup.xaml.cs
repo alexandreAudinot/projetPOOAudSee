1de51f2dedcce630ee0d3039f0e063ea31ca023a
Dan <dan.seeruttun@gmail.com>
2015-01-16 18:30:06 +0100

    change text

--- WpfDisplay/HelpPopup.xaml.cs
+++ WpfDisplay/HelpPopup.xaml.cs
@@ -23,19 +23,19 @@ namespace WpfDisplay
         {
             InitializeComponent();
             textBloc.Text =
-                "Le but du jeu est d'avoir le plus d'anneaux. Plusieurs unités sur la même case ne génère qu'un seul anneau. \n" +
+                "Le but du jeu est d'avoir le plus d'anneaux. Plusieurs unités sur la même case ne génère qu'un seul anneau. \n\n" +
                 "Le coût de déplacement varie selon les cases et les unités. En général, il est de 1.\n" +
                 "Mais il est différent dans les cas suivants.\n" +
                 "Les elfes ne peuvent pas traverser les déserts.\n" +
                 "Les elfes ne payent que 0.5 depl. en traversant une forêt.\n" +
                 "Les orques ne payent que 0.5 depl. en traversant une plaine.\n" +
                 "Les nains ne payent que 0.5 depl. en traversant une plaine.\n" +
-                "Les nains peuvent voyager gratuitement dans les montagnes.\n" +
+                "Les nains peuvent voyager gratuitement dans les montagnes.\n\n" +
                 "Le coût d'une attaque est toujours de 1.\n" +
                 "Seuls les orques génèrent un anneau quand ils tuent une unité adverse, mais ils n'acquièrent pas d'anneaux sur les cases Forêt.\n" +
-                "Les nains n'acquièrent pas de points dans les plaines.\n" +
+                "Les nains n'acquièrent pas de points dans les plaines.\n\n" +
                 "Les elfes ont 50% de chance de fuir un combat perdu (où ils devaient mourir), il se replient sur une case adjacente accessible.\n" +
-                "Dans le cas inverse, l'unité mourra)\n" +
+                "(Dans le cas inverse, l'unité mourra)\n\n" +
                 "Il est conseillé d'éparpiller au maximum ses unités, en attaquant l'adversaire dès que possible.\n" +
                 "L'évaluation du nombre d'anneaux est lancé lors de l'activation du sort de Saruman, ou si un des joueurs perd la partie avant.\n";
    
